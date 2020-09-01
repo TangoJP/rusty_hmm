6f07c0c108373a6feb8d5706503f8bfde7331ec0
R <ryomarcher@gmail.com>
2020-09-01 00:46:44 -0400

    Added viterbi function by modifying forward function. Additionally traceback function was added and tested.

--- src/hmm.rs
+++ src/hmm.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+// use std::collections::HashMap;
 use std::vec::Vec;
 use ndarray::{Array2, s, Axis};
 
@@ -48,28 +48,104 @@ pub fn forward(obs:&Vec<u8>, init_dist: &Vec<f64>, trans_mat: &Array2<f64>, emit
 }
 
 
-struct HMM {
-    states: Vec<String>,                            // set of state names
-    // state_string2index_map: HashMap<String, u8>,    // hashmap to convert state names to numerical index
-    // obs_string2index_map: HashMap<String, u8>,      // hashmap to convert observation names to numerical index
 
-    init_state_distribution: Vec<f64>,              // Initial distribution of states
-    trans_mat: Array2<f64>,                         // state transition probability matrix
-    emit_mat: Array2<f64>,                          // emission probability matrix
-}
+pub fn viterbi(obs:&Vec<u8>, init_dist: &Vec<f64>, trans_mat: &Array2<f64>, emit_mat: &Array2<f64>) -> (Array2<f64>, Array2<usize>) /*f64*/ {
+    let len_obs = obs.len();                    // length of observations
+    let num_states = trans_mat.shape()[0];      // number of possible states
+    let mut v_mat = Array2::<f64>::zeros((num_states, len_obs));  // viterbi matrix
+    let mut bp_mat = Array2::<usize>::ones((num_states, len_obs));  // backpointer matrix
 
-impl HMM {
+    // if the shape doesn't match, raise exception
+    if emit_mat.shape()[0] != num_states {
+        panic!{"Number of rows in emit_mat does not match with that of trans_mat."}
+    };
 
-    pub fn new() -> HMM {
-        HMM {
-            states: Vec::<String>::new(),
-            // state_string2index_map = HashMap::new(),
-            init_state_distribution: Vec::<f64>::new(),
-            trans_mat: Array2::<f64>::zeros((0, 0)),
-            emit_mat: Array2::<f64>::zeros((0, 0)),
+    // if sume along the row for emit_mat does not equal 1 { panic! }
+
+    // initialize
+    for ind_state in 0..num_states {
+        v_mat[[ind_state, 0]] = init_dist[ind_state] * emit_mat[[ind_state, obs[0] as usize]];
+        bp_mat[[ind_state, 0]] = 0;
+    };
+
+
+    for ind_obs in 1..len_obs {                     // for each observation along the observations sequence
+        for ind_curr_state in 0..num_states {       // for each state
+            // calculate the probability of seeing the observation obs[ind_obs] for state ind_current_state as the max probabilities
+            // of coming from all potential paths.
+            let mut vprob_temp = 0.0;
+            let mut bp_ind_temp = 0;           
+            for ind_prev_state in 0..num_states{
+                let v_ = v_mat[[ind_prev_state, ind_obs-1]] * trans_mat[[ind_prev_state, ind_curr_state]] * emit_mat[[ind_curr_state, obs[ind_obs] as usize]];
+                if v_ > vprob_temp {
+                    vprob_temp = v_;
+                    bp_ind_temp = ind_prev_state;
+                }
+            }
+
+            v_mat[[ind_curr_state, ind_obs]] = vprob_temp;
+            bp_mat[[ind_curr_state, ind_obs]] = bp_ind_temp;
+        }
+    }
+
+    let mut best_path_prob = 0.0;
+    let mut best_bp = 0;
+    for (i, row) in v_mat.axis_iter(Axis(0)).enumerate() {
+        let prob_temp = row[len_obs - 1];
+        if  prob_temp > best_path_prob {
+            best_path_prob = prob_temp;
+            best_bp = i;
         }
     }
 
 
+    // forward
+    (v_mat, bp_mat)
+}
+
+pub fn traceback_viterbi(backpointer: &Array2<usize>, start: usize) -> Vec<usize> {
+    let path_length = backpointer.shape()[1];
+    let mut path = Vec::<usize>::with_capacity(path_length);
+
+    let mut i = path_length - 1;
+    let mut j = start;
+    path.push(start);
+    while i > 0 {
+        let prev_state = backpointer[[j, i]];
+        path.push(prev_state);
+        j = prev_state;
+        i -= 1;
+    }
+
+    path.reverse();
+
+    path
 
 }
+
+
+// struct HMM {
+//     states: Vec<String>,                            // set of state names
+//     // state_string2index_map: HashMap<String, u8>,    // hashmap to convert state names to numerical index
+//     // obs_string2index_map: HashMap<String, u8>,      // hashmap to convert observation names to numerical index
+
+//     init_state_distribution: Vec<f64>,              // Initial distribution of states
+//     trans_mat: Array2<f64>,                         // state transition probability matrix
+//     emit_mat: Array2<f64>,                          // emission probability matrix
+// }
+
+// impl HMM {
+
+//     pub fn new() -> HMM {
+//         HMM {
+//             states: Vec::<String>::new(),
+//             // state_string2index_map = HashMap::new(),
+//             init_state_distribution: Vec::<f64>::new(),
+//             trans_mat: Array2::<f64>::zeros((0, 0)),
+//             emit_mat: Array2::<f64>::zeros((0, 0)),
+//         }
+//     }
+
+
+
+// }
--- tests/test_hmm.rs
+++ tests/test_hmm.rs
@@ -1,12 +1,59 @@
 use rusty_hmm::hmm;
 use ndarray::{Axis, arr2};
 
+
 #[test]
 // #[ignore]
+fn test_viterbi() {
+    // thinking coin flip with a normal (state 0) and fixed coin (state 1)
+    // assume observation 1 = head, 0 = tail
+    let obs = vec![0u8, 1u8, 1u8, 1u8, 0u8, 0u8, 0u8, 1u8, 1u8, 0u8, 0u8, 0u8, 1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8];
+    let mut init_dist = Vec::<f64>::new();
+    init_dist.push(0.5);
+    init_dist.push(0.5);
+
+    let trans_mat = arr2(&[
+        [0.55, 0.45],
+        [0.45, 0.55]
+    ]);
+    let emit_mat = arr2(&[
+        [0.5, 0.5],
+        [0.65, 0.35]
+    ]);
+
+    let (v, b) = hmm::viterbi(
+         &obs, 
+         &init_dist, 
+         &trans_mat, 
+         &emit_mat);
+    
+    let mut best_path_prob = 0.0;
+    let mut best_bp = 0;
+    for (i, row) in v.axis_iter(Axis(0)).enumerate() {
+        let prob_temp = row[obs.len() - 1];
+        if  prob_temp > best_path_prob {
+            best_path_prob = prob_temp;
+            best_bp = i;
+        }
+    }
+
+    let bestpath = hmm::traceback_viterbi(&b, best_bp);
+
+    println!("Viterbi Matrix with shape{:?}\n{:?}", v.shape(), v);
+    println!("Backtrace Matrix with shape{:?}\n{:?}", b.shape(), b);
+
+    println!("Viterbi probability = {:?}", best_path_prob);
+    println!("Best Path = {:?}", bestpath);
+
+
+}
+
+#[test]
+#[ignore]
 fn test_forward() {
     // thinking coin flip with a normal (state 0) and fixed coin (state 1)
     // assume observation 1 = head, 0 = tail
-    let obs = vec![0u8, 1u8, 1u8, 1u8, 0u8, 0u8, 0u8, 1u8, 0u8, 1u8, 0u8, 0u8, 1u8, 0u8, 1u8];
+    let obs = vec![0u8, 1u8, 1u8, 1u8, 0u8, 0u8, 0u8, 1u8, 1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8];
     let mut init_dist = Vec::<f64>::new();
     init_dist.push(0.65);
     init_dist.push(0.35);
